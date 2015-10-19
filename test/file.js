234b74044f38c4008c3c9dcfbb50802e0e287dfe
Dominic Barnes <dominic@dbarnes.info>
2015-10-18 23:51:09 -0700

    adding file helpers for working with tree

--- test/file.js
+++ test/file.js
@@ -3,6 +3,7 @@
 
 let assert = require('chai').assert;
 let File = require('../lib/file');
+let Tree = require('../lib/tree');
 
 describe('File()', function () {
   it('should be a constructor function', function () {
@@ -18,5 +19,101 @@ describe('File()', function () {
   it('should set the type property', function () {
     let file = new File('a.js');
     assert.strictEqual(file.type, 'js');
+  });
+
+  describe('#isSource()', function () {
+    // a -> b
+    let tree = new Tree();
+    let a = tree.addFile('a');
+    let b = tree.addFile('b');
+    tree.addDependency('a', 'b');
+
+    it('should return true if the file is a source', function () {
+      assert.isTrue(a.isSource());
+    });
+
+    it('should return false if the file is not a source', function () {
+      assert.isFalse(b.isSource());
+    });
+  });
+
+  describe('#hasDependency(child)', function () {
+    // a -> b
+    let tree = new Tree();
+    let a = tree.addFile('a');
+    tree.addFile('b');
+    tree.addDependency('a', 'b');
+
+    it('should return true file has the given child dependency', function () {
+      assert.isTrue(a.hasDependency('b'));
+    });
+
+    it('should return false if the file does not have the given child dependency', function () {
+      assert.isFalse(a.hasDependency('c'));
+    });
+  });
+
+  describe('#addDependency(child)', function () {
+    it('should add the child as a new dependency', function () {
+      let tree = new Tree();
+      let a = tree.addFile('a');
+      a.addDependency('b');
+
+      assert.isTrue(tree.hasFile('b'));
+    });
+
+    it('should return the newly added dependency file', function () {
+      let tree = new Tree();
+      let a = tree.addFile('a');
+      let b = a.addDependency('b');
+
+      assert.strictEqual(b, tree.getFile('b'));
+    });
+  });
+
+  describe('#removeDependency(child)', function () {
+    it('should remove the child as a dependency', function () {
+      // a -> b
+      let tree = new Tree();
+      let a = tree.addFile('a');
+      a.addDependency('b');
+      a.removeDependency('b');
+
+      assert.isFalse(tree.hasDependency('a', 'b'));
+    });
+  });
+
+  describe('#dependencies(recursive)', function () {
+    // a -> b -> c -> d
+    let tree = new Tree();
+    let a = tree.addFile('a');
+    let b = a.addDependency('b');
+    let c = b.addDependency('c');
+    c.addDependency('d');
+
+    it('should return the direct descendents', function () {
+      assert.deepEqual(b.dependencies(), [ 'c' ]);
+    });
+
+    it('should return the entire dependency chain', function () {
+      assert.deepEqual(b.dependencies(true), [ 'c', 'd' ]);
+    });
+  });
+
+  describe('#dependants(recursive)', function () {
+    // a -> b -> c -> d
+    let tree = new Tree();
+    let a = tree.addFile('a');
+    let b = a.addDependency('b');
+    let c = b.addDependency('c');
+    c.addDependency('d');
+
+    it('should return the direct descendents', function () {
+      assert.deepEqual(c.dependants(), [ 'b' ]);
+    });
+
+    it('should return the entire dependency chain', function () {
+      assert.deepEqual(c.dependants(true), [ 'b', 'a' ]);
+    });
   });
 });
